4021e8a9e9b8a46cc773e712d89b874049b16faf
chicco4 <chiccotto4@gmail.com>
2022-07-25 19:42:46 +0200

    simple done

---
+++ canvas/08-fireworks/canvas.js
@@ -0,0 +1,99 @@
+/* UTILITY FUNCTIONS */
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function distance(x1, y1, x2, y2) {
+  const xDist = x2 - x1;
+  const yDist = y2 - y1;
+
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+}
+
+/* MAIN STUFF */
+
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
+
+const colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
+
+addEventListener("resize", () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  init();
+});
+
+// Objects
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
+
+// Implementation
+let particles;
+function init() {
+  particles = [];
+}
+
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  particles.forEach((particle) => {
+    particle.update();
+  });
+}
+
+init();
+animate();
+
+addEventListener("click", (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+  console.log("click " + mouse.x + " " + mouse.y);
+
+  const particleCount = 400;
+  const angleIncrement = (Math.PI * 2) / particleCount;
+
+  for (let i = 0; i < 400; i++) {
+    particles.push(
+      new Particle(mouse.x, mouse.y, 5, "red", {
+        x: Math.cos(angleIncrement * i) * Math.random(),
+        y: Math.sin(angleIncrement * i) * Math.random(),
+      })
+    );
+  }
+});
