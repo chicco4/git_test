8890b2f8e4d05cd5523bff11b06563c535be84c1
chicco4 <chiccotto4@gmail.com>
2022-07-25 19:56:50 +0200

    done

--- canvas/08-fireworks/canvas.js
+++ canvas/08-fireworks/canvas.js
@@ -44,23 +44,33 @@ class Particle {
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
+    this.alpha = 1;
   }
 
   draw() {
+    c.save();
+    c.globalAlpha = this.alpha;
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     c.fillStyle = this.color;
     c.fill();
     c.closePath();
+    c.restore();
   }
 
   update() {
     this.draw();
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.velocity.y += gravity;
     this.x += this.velocity.x;
     this.y += this.velocity.y;
+    this.alpha -= 0.005;
   }
 }
 
+const gravity = 0.01;
+const friction = 0.99;
 // Implementation
 let particles;
 function init() {
@@ -70,30 +80,42 @@ function init() {
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = "rgba(0,0,0,0.05)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
-  particles.forEach((particle) => {
-    particle.update();
+  particles.forEach((particle, i) => {
+    if (particle.alpha > 0) {
+      particle.update();
+    } else {
+      particles.splice(i, 1);
+    }
   });
 }
 
 init();
 animate();
 
+// Alt Gr + apostrofe = `
+
 addEventListener("click", (event) => {
   mouse.x = event.clientX;
   mouse.y = event.clientY;
-  console.log("click " + mouse.x + " " + mouse.y);
 
   const particleCount = 400;
   const angleIncrement = (Math.PI * 2) / particleCount;
 
   for (let i = 0; i < 400; i++) {
     particles.push(
-      new Particle(mouse.x, mouse.y, 5, "red", {
-        x: Math.cos(angleIncrement * i) * Math.random(),
-        y: Math.sin(angleIncrement * i) * Math.random(),
-      })
+      new Particle(
+        mouse.x,
+        mouse.y,
+        3,
+        `hsl(${Math.random() * 360}, 50%,50%)`,
+        {
+          x: Math.cos(angleIncrement * i) * Math.random() * 8,
+          y: Math.sin(angleIncrement * i) * Math.random() * 8,
+        }
+      )
     );
   }
 });
